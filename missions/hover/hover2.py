79bcbe931b417400a931d515a7d68456751f93b0
Tylorgonza <89476154+Tylorgonza@users.noreply.github.com>
2022-05-27 12:47:11 -0500

    Update hover2.py

--- missions/hover/hover2.py
+++ missions/hover/hover2.py
@@ -9,6 +9,8 @@ from skymission.concurrency import tick
 from skymission.mission import Mission
 from skymission.mission import callback
 from skymission.mission import panic
+from datetime import date
+
 
 
 class LocationMessage(BaseMessage):
@@ -51,7 +53,9 @@ class HoverMission(Mission):
     """
 	CREATING INSTANCE OF TXT SHEET TO WRITE TO WHILE GRABBING DATA DURING THIS MISSION
     """
-    f = open("log_"+today(),'w')
+    date = date.today()
+    filename = "log_{date}"
+    f = open(filename,'w')
     def __init__(self, fc_addr, log_file):
         """
         Create a HoverMission, which is started as soon as the class is instantiated.
@@ -123,7 +127,7 @@ class HoverMission(Mission):
 	    """
 	    logging first location
 	    """
-            nextlat, nextlon = 29.71758978560598, -95.4055873521442
+            nextlat, nextlon = 29.71587997076222, -95.40949629024838
 
             self.log.debug('Navigating to waypoint: ({lat}, {lon})'.format(lat=nextlat,lon=nextlon,))
             self.dc.goto(coords=(nextlat, nextlon), altitude=alt, airspeed=2)
@@ -131,10 +135,9 @@ class HoverMission(Mission):
 
             location = self.dc.read_gps()
             self.log.debug('Arrived! Current location: ({lat}, {lon})'.format(lat=location.lat,lon=location.lon,))
-
+            f.write('lat:{lat},lon:{lon},Alt:{alt}')
             time.sleep(3)
-	    	f.write("first location (lat,lon): {lat},{lon} Alt: {alt}  \t")
-	    	f.write()
+	    	
 
             # Moving to location 2
             """
@@ -142,7 +145,7 @@ class HoverMission(Mission):
             """
 
 
-            nextlat, nextlon = 29.717660833596735, -95.40562356196683
+            nextlat, nextlon = 29.716048881306033, -95.4094887158422
 
             self.log.debug('Navigating to waypoint: ({lat}, {lon})'.format(lat=nextlat,lon=nextlon,))
             self.dc.goto(coords=(nextlat, nextlon), altitude=alt, airspeed=2)
@@ -152,13 +155,13 @@ class HoverMission(Mission):
             self.log.debug('Arrived! Current location: ({lat}, {lon})'.format(lat=location.lat,lon=location.lon,))
 
             time.sleep(3)
-            f.write("second location (lat,lon): {lat},{lon} Alt: {alt} \t")
+            f.write("second location lat: {lat} lon: {lon} Alt: {alt} \t")
 
 
 
             # Moving to location 3
 
-            nextlat, nextlon = 29.71772838737737, -95.40566513620766
+            nextlat, nextlon = 29.716231223497598, -95.40946649704692
 
             self.log.debug('Navigating to waypoint: ({lat}, {lon})'.format(lat=nextlat,lon=nextlon,))
             self.dc.goto(coords=(nextlat, nextlon), altitude=alt, airspeed=2)
@@ -168,11 +171,51 @@ class HoverMission(Mission):
             self.log.debug('Arrived! Current location: ({lat}, {lon})'.format(lat=location.lat,lon=location.lon,))
 
             time.sleep(3)
+            f.write("third location lat: {lat} lon: {lon} Alt: {alt} \t")
+
+
+            # Moving to location 4
+
+            nextlat,nextlon = 29.71641642334013, -95.40938623970409
+
+            self.log.debug('Navigating to waypoint; ({lat},{lon})'.format(lat=nextlat,lon=nextlon,))
+            self.dc.goto(coords=(nextlat,nextlon),altitude=alt,airspeed=2)
+            self.log.debug('Navigation to waypoint complete')
+
+            location = self.dc.read_gps()
+            self.log.debug('Arrived! Current location: ({lat},{lon})'.format(lat=location.lat,lon=location.lon))
+
+            time.sleep(3)
+            f.write("fourth location lat: {lat} lon: {lon} Alt: {alt} ")
+
+
+            #Moving to location 5
+
+            nextlat,nextlat = 29.71659056000551,-95.40929848482654
+
+            self.log.debug('Navigating to waypoint: ({lat}, {lon})'.format(lat=nextlat,lon=nextlon,))
+            self.dc.goto(coords=(nextlat,nextlon),altitude=alt,airspeed=2)
+            self.log.debug('Navigation to waypoint complete')
+            location = self.dc.read_gps()
+            self.log.debug('Arrived! Current location: ({lat},{lon})'.format(lat=location.lat,lon=location.lon))
+
+            time.sleep(3)
+            f.write("fifth location lat: {lat} lon: {lon} Alt: {alt} ")
+
+            #Moving to location 6
+            nextlat,nextlat = 29.716750897595286,-95.40915175305562
+
+            self.log.debug('Navigating to waypoint: ({lat}, {lon})'.format(lat=nextlat,lon=nextlon,))
+            self.dc.goto(coords=(nextlat,nextlon),altitude=alt,airspeed=2)
+            self.log.debug('Navigation to waypoint complete')
+            location = self.dc.read_gps()
+            self.log.debug('Arrived! Current location: ({lat},{lon})'.format(lat=location.lat,lon=location.lon))
+
 
             self.log.debug('Hovering for: {hover_time} seconds'.format(hover_time=hover_time))
             time.sleep(hover_time)
-            #third location being logged
-            f.write("third location (lat,lon): {lat},{lon} Alt: {alt} Hovertime: {hover_time} \t")
+            
+            f.write("fifth location lat: {lat} lon: {lon} Alt: {alt} Hovertime: {hover_time} \t")
 
 
             self.log.info('Hovering complete; begin landing')
